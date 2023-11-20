57c7322743f71b61a2f8a1cb52b69dd339599180
Androphic <estereos@gmail.com>
2023-11-20 23:45:36 +0200

    Comment Header correction
    Comment Header correction

--- Base64_Encrypted_C/src/b64_encryptor.h
+++ Base64_Encrypted_C/src/b64_encryptor.h
@@ -4,7 +4,7 @@
  Author      : Tofig Kareemov
  Version     :
  Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Description : Base64 Encryptor in C, Ansi-style
  ============================================================================
  */
 
